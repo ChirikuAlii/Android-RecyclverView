1bb8b9ec917ac09640c98b94507f2ef70f9371e8
ChirikuAlii <chiriku.ali.rhmat@gmail.com>
2022-10-21 20:04:30 +0700

    add view type recyclver view

--- app/src/main/java/com/chirikualii/materi_recyclerview/MainActivity.kt
+++ app/src/main/java/com/chirikualii/materi_recyclerview/MainActivity.kt
@@ -8,12 +8,18 @@ import com.chirikualii.materi_recyclerview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+    lateinit var adapter: ListProfileAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //TODO setup adapter
+
+        adapter = ListProfileAdapter()
+
+        adapter.addList(DataDummy.listProfile)
+
+        binding.rvProfile.adapter = adapter
     }
 }
